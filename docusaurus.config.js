736dd6c22e48956252886d5adf44dc4df3031dd9
Angel <angelCL0@outlook.com>
2020-09-09 20:18:50 -0400

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+module.exports = {
+  title: "BuildKit",
+  tagline: "The ultimate React & Next component library_",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "BuildKit",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/Scan.png",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/anyelos/buildkit",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/",
+            },
+            {
+              label: "Instagram",
+              href: "https://www.instagram.com/angelvzur/",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/angelVZUR",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/anyelos/buildkit",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} BuildKit, Inc.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
